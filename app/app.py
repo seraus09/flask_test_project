b726dce0cfcac761ea28f10da76cd9293c63660a
ZharPtica27 <sera.ua09@gmail.com>
2020-06-16 13:59:18 +0300

    update

--- app/app.py
+++ app/app.py
@@ -10,20 +10,27 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 Bootstrap(app)
 
-def ping(ip):
-    cmd = f"ping -c 4 {ip}"
-    res = os.popen(cmd).read()
-    data = res.splitlines()
-    results = (data[0],data[7],data[8])
-    return results
+class MainPage():
+
+    def __init__(self, ipaddr):
+        self.ipaddr = ipaddr
+
+    def geoIP(self):
+        ip = (self.ipaddr)
+        url = requests.get(f'http://api.db-ip.com/v2/free/{ip}')
+        if url.status_code == 200:
+            return url.text
+        else:
+            return "Error"
 
 @app.route('/' , methods=['GET', 'POST'])
 def index():
     ip = None
     form = forms.TypeIP()
     if form.validate_on_submit():
-        ip = form.ip.data
-    return render_template('index.html', form=form, ip=ip, data=ping(ip))
+        ip = MainPage(form.ip.data)
+        data = ip.geoIP()
+    return render_template('index.html', form=form, ip=ip, data=data )
 
 
 
