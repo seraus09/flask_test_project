1dee7947cad35c87a42ebd8a51476d3bbda58df5
ZharPtica27 <sera.ua09@gmail.com>
2020-06-18 20:26:02 +0300

    18.06

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.6
 
-RUN pip3 install Flask==0.10.1 requests==2.5.1  requests==2.5.1 redis==2.10.3 Flask-Bootstrap==3.3.7 Flask-WTF
+RUN pip3 install Flask==0.10.1 requests==2.5.1  requests==2.5.1 redis==2.10.3 Flask-Bootstrap==3.3.7 Flask-WTF flask-moment
 
 RUN groupadd -r uwsgi && useradd -r -g uwsgi uwsgi
 WORKDIR /app
--- app/app.py
+++ app/app.py
@@ -1,14 +1,17 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, url_for, flash, request
 from flask import render_template
 from flask_bootstrap import Bootstrap
 import requests
 import forms
+from datetime import datetime
 import os
+from flask_moment import Moment
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 Bootstrap(app)
+moment = Moment(app)
 
 class MainPage():
 
@@ -30,7 +33,16 @@ def index():
     if form.validate_on_submit():
         ip = MainPage(form.ip.data)
         data = ip.geoIP()
-    return render_template('index.html', form=form, ip=ip, data=data )
+        res = dict(eval(data))
+        addr = res.get("ipAddress")
+        country = res.get("continentName")
+        region = res.get("stateProv")
+        city = res.get("city")
+        real_ip = request.remote_addr
+    else:
+        return  render_template('index.html', form=form, current_time=datetime.utcnow(),real_ip=request.remote_addr)
+
+    return render_template('main.html', current_time=datetime.utcnow(),real_ip=real_ip, form=form, ip=ip, addr=addr, country=country, region=region, city=city)
 
 
 
