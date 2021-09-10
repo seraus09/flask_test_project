6abb0caf8f26eb3b4a424c2bad2c43d2346ea888
Sergey <sera.ua09@gmail.com>
2021-09-10 11:33:34 +0300

    fix whois component

--- app/api/views.py
+++ app/api/views.py
@@ -14,7 +14,7 @@ import re
 
 
 
-api_bp = Blueprint('api_v1', __name__)
+api_bp = Blueprint("api_v1", __name__)
 api_v1 = Api(api_bp)
 
 
@@ -25,13 +25,13 @@ class CleanHost():
 
     def get_clean_hostname(self):
         url = self.host
-        if re.match('http:|https:|ftp:', str(url)):
+        if re.match("http:|https:|ftp:", str(url)):
             hostname = socket.getaddrinfo(urlparse(url).hostname, 443, proto=socket.IPPROTO_TCP)
-            ip_candidates = re.findall(r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b', str(hostname))
-            clean_string = str(ip_candidates)[0:17].rstrip(']').rstrip("'").lstrip("[").lstrip("'").lstrip("b").rstrip(",").rstrip("'")
-            return str(clean_string.encode('idna')).lstrip('b').lstrip("'").rstrip("'")
+            ip_candidates = re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", str(hostname))
+            clean_string = str(ip_candidates)[0:17].rstrip("]").rstrip("'").lstrip("[").lstrip("'").lstrip("b").rstrip(",").rstrip("'")
+            return str(clean_string.encode("idna")).lstrip("b").lstrip("'").rstrip("'")
         else:
-            return str(url.encode('idna')).lstrip('b').rstrip("'").lstrip("'")
+            return str(url.encode("idna")).lstrip("b").rstrip("'").lstrip("'")
 
 
 
@@ -40,7 +40,7 @@ class GetGeo(Resource):
         API_KEY = current_app.config["API_KEY"]
         ip =  CleanHost(host).get_clean_hostname()
         try:
-            url = requests.get(f'http://api.ipstack.com/{ip}?access_key={API_KEY}')
+            url = requests.get(f"http://api.ipstack.com/{ip}?access_key={API_KEY}")
             if url.status_code == 200:
                 logger.debug(url.json())
                 return url.json()
@@ -52,36 +52,36 @@ class WhoisInfo(Resource):
     def get(self,host):
         hostname = urlparse(host).hostname
         try:
-            if re.match('http:|https:', host):
-                if re.match(r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b', str(hostname)):
+            if re.match("http:|https:", host):
+                if re.match(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", str(hostname)):
                     return IPWhois(hostname).lookup_whois()['nets'][0], 200
                 else:
                     return whois.query(hostname).__dict__,200
             elif hostname is None:
-                if re.match(r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b', str(host)):
+                if re.match(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", str(host)):
                     return IPWhois(host).lookup_whois()['nets'][0], 200
                 else:
                     whois_data = dict()
                     time_data = dict()
                     for x,y in whois.query(host).__dict__.items():                    
-                        if x == 'creation_date' or x == 'expiration_date' or x == 'last_updated' or x == 'name_servers':
+                        if x == "creation_date" or x == "expiration_date" or x == "last_updated" or x == "name_servers":
                             time_data[x]=str(y)
                         else:
                             whois_data[x]=y
-                    return {'main_data': whois_data, 'data': time_data} ,200
+                    return {"main_data": whois_data, "data": time_data} ,200
 
             else:
-                return {'error':'Not information about this zone'}, 400
+                return {"error":"Not information about this zone"}, 400
         except Exception as error:
             logger.error(error)
-            return {'error':'Not information about this zone'}, 400
+            return {"error":"Not information about this zone"}, 400
 
 
 
 
 
-api_v1.add_resource(GetGeo, '/api/geo/<string:host>')
-api_v1.add_resource(WhoisInfo, '/api/whois/<string:host>')
+api_v1.add_resource(GetGeo, "/api/geo/<string:host>")
+api_v1.add_resource(WhoisInfo, "/api/whois/<string:host>")
 
 
 
--- frontend/src/components/DomainWhois.js
+++ frontend/src/components/DomainWhois.js
@@ -16,7 +16,7 @@ const DomainWhois = (props) => {
   const [whois,setWhois] = useState([])
   const [loading,setLoading] = useState(false)
   
-  async function  getApiRes(host){
+  async function  getApiRes(){
     setLoading(true)
     await instance.get(`/api/whois/${props.host}`).then((resp) => {
        const allInfo = resp.data;
@@ -27,6 +27,7 @@ const DomainWhois = (props) => {
 
    });
   }
+  console.log(JSON.stringify(whois,null,2))
   useEffect(() => {
       getApiRes(props.host)
       },[props.host]);
@@ -39,39 +40,39 @@ const DomainWhois = (props) => {
           <tbody>    
             <tr>
               <td>Name:</td>
-              <td>{whois.main_data.name}</td>
+              <td>{whois?.main_data?.name}</td>
             </tr>
             <tr>
               <td>Registrar:</td>
-              <td>{whois.main_data.registrar}</td>
+              <td>{whois?.main_data?.registrar}</td>
             </tr>
             <tr>
               <td>Registrant country:</td>
-              <td>{whois.main_data.registrant_country}</td>
+              <td>{whois?.main_data?.registrant_country}</td>
             </tr>
             <tr>
               <td>Creation date:</td>
-              <td>{whois.data.creation_date}</td>
+              <td>{whois?.data?.creation_date}</td>
             </tr>
             <tr>
               <td>Expiration date:</td>
-              <td>{whois.data.expiration_date}</td>
+              <td>{whois?.data?.expiration_date}</td>
             </tr>
             <tr>
               <td>Last update:</td>
-              <td>{whois.data.last_updated}</td>
+              <td>{whois?.data?.last_updated}</td>
             </tr>
               <tr>
                <td>Status:</td>
-              <td>{whois.main_data.status}</td>
+              <td>{whois?.main_data?.status}</td>
             </tr>
             <tr>
               <td>Statuses:</td>
-              <td>{whois.main_data.statuses}</td>
+              <td>{whois?.main_data?.statuses}</td>
             </tr>
             <tr>
               <td>Name servers:</td>
-              <td>{whois.data.name_servers}</td>
+              <td>{whois?.data?.name_servers}</td>
             </tr>
 
             </tbody>
@@ -79,6 +80,7 @@ const DomainWhois = (props) => {
         
         </div>
       )
+      
     }
 
 export default DomainWhois;
